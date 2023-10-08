f6343fe0b94b563d5a28d27f1e66f5e055d20217
Marcin Nowak-Liebiediew <marcin.liebiediew@dfinity.org>
2023-10-08 16:03:34 +0200

    my keymap

---
+++ config/keypos.h
@@ -0,0 +1,32 @@
+#pragma once
+// P stands for position
+#define P_Q LT1
+#define P_W LT2
+#define P_F LT3
+#define P_P LT4
+#define P_G LT5
+#define P_J RT0
+#define P_L RT1
+#define P_U RT2
+#define P_Y RT3
+#define P_SEMICOLON RT4
+#define P_A LM1
+#define P_R LM2
+#define P_S LM3
+#define P_T LM4
+#define P_D LM5
+#define P_H RM0
+#define P_N RM1
+#define P_E RM2
+#define P_I RM3
+#define P_O RM4
+#define P_Z LB1
+#define P_X LB2
+#define P_C LB3
+#define P_V LB4
+#define P_B LB5
+#define P_K RB0
+#define P_M RB1
+#define P_COMMA RB2
+#define P_DOT RB3
+#define P_SLASH RB4
