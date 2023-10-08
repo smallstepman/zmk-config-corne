cdaa67fbf07121d46d4bb28f0e4d96e031f03892
smallstepman <smallstepman@users.noreply.github.com>
2023-10-08 18:32:52 +0200

    keymap

--- config/keypos.h
+++ config/keypos.h
@@ -1,5 +1,5 @@
 #pragma once
-// P stands for position
+// P stands for position([])
 #define P_Q LT4
 #define P_W LT3
 #define P_F LT2
@@ -10,21 +10,21 @@
 #define P_U RT2
 #define P_Y RT3
 #define P_SEMICOLON RT4
-#define P_A LM5
-#define P_R LM4
-#define P_S LM3
-#define P_T LM2
-#define P_D LM1
+#define P_A LM4
+#define P_R LM3
+#define P_S LM2
+#define P_T LM1
+#define P_D LM0
 #define P_H RM0
 #define P_N RM1
 #define P_E RM2
 #define P_I RM3
 #define P_O RM4
-#define P_Z LB1
-#define P_X LB2
-#define P_C LB3
-#define P_V LB4
-#define P_B LB5
+#define P_Z LB4
+#define P_X LB3
+#define P_C LB2
+#define P_V LB1
+#define P_B LB0
 #define P_K RB0
 #define P_M RB1
 #define P_COMMA RB2
